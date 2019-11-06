110cb51b7b3643ff78dbd00420861c338e4c6817
Nemuel <lim.nemuel.22@gmail.com>
2019-11-06 15:29:35 +0800

    Create animals routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/69', function (req, res, next) {
+  res.send({
+    name: "test"
+  });
+});
+
+router.post('/', function (req, res, next) {
+  res.send({
+    name: "test"
+  });
+});
+
 
 module.exports = router;
